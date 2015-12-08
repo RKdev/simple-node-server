34f4952614808477f2c6da7bb70ee743f16e7553
RKdev <rkdevmail@gmail.com>
2015-12-08 00:16:44 -0600

    ouputs percentage completion of installer

--- install-simple-node-server.sh
+++ install-simple-node-server.sh
@@ -1,4 +1,4 @@
 curl -O https://nodejs.org/dist/v4.2.3/node-v4.2.3.pkg
-installer -pkg ./node-v4.2.3.pkg -target /
+installer -verboseR -pkg ./node-v4.2.3.pkg -target / | grep "%"
 mkdir ~/simple-node-server
 cp -R simple-node-server/* ~/simple-node-server
